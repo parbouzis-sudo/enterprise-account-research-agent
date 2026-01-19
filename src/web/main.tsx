e8497b84a7b46cc05cd32c49e222571d9fd4d3d9
parbouzis-sudo <parbouzis@gmail.com>
2026-01-18 20:59:48 -0500

    Add main entry point for React application

---
+++ src/web/main.tsx
@@ -0,0 +1,17 @@
+/**
+ * React app entry point
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./index.css";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("Root element not found");
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
